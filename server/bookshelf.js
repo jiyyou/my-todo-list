a431917d171684d5b1780ab07253bf5248727bba
Jiyo <ji.yyou@gmail.com>
2020-07-17 15:09:16 -0400

    bookshelf updated

--- server/bookshelf.js
+++ server/bookshelf.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
+const { JAWSDB_URL } = process.env;
+
 const knex =
   process.env.NODE_ENV === "production"
     ? require("knex")(require("./knexfile").production)
     : require("knex")(require("./knexfile").development);
-    
+
 const bookshelf = require("bookshelf")(knex);
 
 module.exports = bookshelf;
